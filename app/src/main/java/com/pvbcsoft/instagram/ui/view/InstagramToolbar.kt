7bac56ef64039a2e59dfeaa1e3700120bbdbd3af
Paulo Victor <127259309+paulovictorbarros@users.noreply.github.com>
2023-09-04 21:00:47 -0300

    Toolbar finalizada

---
+++ app/src/main/java/com/pvbcsoft/instagram/ui/view/InstagramToolbar.kt
@@ -0,0 +1,62 @@
+package com.pvbcsoft.instagram.ui.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.pvbcsoft.instagram.R
+
+@Composable
+fun InstagramToolbar() {
+    Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .height(56.dp)
+        ) {
+
+            Text(
+                text = "Instagram",
+                modifier = Modifier
+                    .weight(1f)
+                    .align(Alignment.CenterVertically),
+                style = MaterialTheme.typography.headlineLarge
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_notification),
+                modifier = Modifier
+                    .size(32.dp)
+                    .padding(end = 8.dp)
+                    .align(Alignment.CenterVertically),
+                contentDescription = "ícone de notificações na barra de Ferramentas"
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_message),
+                modifier = Modifier
+                    .size(32.dp)
+                    .padding(start = 8.dp)
+                    .align(Alignment.CenterVertically),
+                contentDescription = "ícone de mensagens na barra de Ferramentas"
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InstagramToolbarPreview() {
+    InstagramToolbar()
+}
